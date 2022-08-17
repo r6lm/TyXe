2f3797d7698e816a29e5e6a23a6fec60f71f8135
r6lm <rdgolaramolina@gmail.com>
2022-08-17 20:20:49 +0100

    feat: Add jobs for test in Eddie

---
+++ jobs/test/s5/s5.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# Grid Engine options (lines prefixed with #$)
+#$ -N BIU-seed5     
+#$ -cwd
+#$ -l h_rt=24:00:00
+#$ -l h_vmem=32G
+#$ -o my_stdout.log
+#$ -e my_stderr.log
+#$ -M ro6lm@outlook.com
+#$ -m beas
+#$ -pe gpu-titanx 1
+
+# Initialise the environment modules
+. /etc/profile.d/modules.sh
+
+# Load conda environment
+module load anaconda
+source activate tyxe
+
+# Run the program
+cd /home/s2110626/diss/TyXe/notebooks 
+python -u ./vcl-MF.py --seed 5
